5f6f68c7eb8f1b9c66ecebe420f1ee7a2c9dd2c4
Edge <48862659+Shigetorum635@users.noreply.github.com>
2022-06-08 11:39:42 +0000

    feat: add everything

--- untube.js
+++ untube.js
@@ -1,1 +1,23 @@
-// I am running out of battery
+const express = require("express");
+const ytdl = require("ytdl-core");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/unblock", (req, res) => {
+  const { video } = req.query;
+  if (!video) return res.status(400);
+  try {
+    ytdl(video).pipe(res);
+  } catch (err) {
+    res.status(400);
+  }
+});
+
+app.listen(3000);
